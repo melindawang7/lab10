099effc78ece92bd46a1ab0712729afc05e2b245
melindawang7 <melindawang@utexas.edu>
2026-03-26 11:57:14 -0500

    updated url

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,8 +10,8 @@ createRoot(document.getElementById('root')).render(
     <AsgardeoProvider
       clientId="J1n0WkCbE1_xQRRwCL_A8TZXyq8a"
       baseUrl="https://api.asgardeo.io/t/mis372tsecurity"
-      signInRedirectURL="http://localhost:5173/"
-      signOutRedirectURL="http://localhost:5173/"
+      signInRedirectURL="https://lab10-frontend-jwib.onrender.com"
+      signOutRedirectURL="https://lab10-frontend-jwib.onrender.com"
       scopes="openid profile"
     >
       <App />
